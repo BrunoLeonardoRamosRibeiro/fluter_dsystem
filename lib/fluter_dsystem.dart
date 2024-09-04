2f4bf4ace7a353b946bd92cd8624882e7f4114ba
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-09-04 17:50:48 -0300

    Exports DS

--- lib/fluter_dsystem.dart
+++ lib/fluter_dsystem.dart
@@ -1,7 +1,16 @@
 library fluter_dsystem;
+// library box_ui;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// Widgets Export
+export 'src/widgets/box_text.dart';
+export 'src/widgets/box_button.dart';
+export 'src/widgets/box_input_field.dart';
+
+// Colors Export
+export 'src/shared/app_colors.dart';
+
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
